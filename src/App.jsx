a93b658bfe99d19f6c125b1f683038ef3df09419
albertsube <120310184+albertsube@users.noreply.github.com>
2023-12-27 16:15:42 +0100

    Revert "Albertsube/login"

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import Explore from './pages/Explore'
 import Profile from './pages/Profile'
@@ -7,20 +7,26 @@ import Register from './pages/Register'
 import { useState } from 'react'
 
 function App() {
-  const [user, setUser] = useState('test')
+  const [user, setUser] = useState(null)
   const [isAuthenticated, setIsAuthenticated] = useState(false)
 
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout user={user} isAuthenticated={isAuthenticated} />}>
-          <Route path="*" element={<Navigate to="/" />} />
-          {isAuthenticated ? (
-            <Route path="/" element={<Navigate to="/explore" />} />
-          ) : (
-            <Route path="/" element={<Navigate to="/login" />} />
-          )}
-          <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} setUser={setUser} />} />
+          <Route index element={<Login />} />
+          <Route
+            path="/login"
+            element={
+              <Login isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} />
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <Register isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} setUser={setUser} />
+            }
+          />
           <Route path="/explore" element={<Explore />} />
           <Route path="/profile" element={<Profile />} />
         </Route>
