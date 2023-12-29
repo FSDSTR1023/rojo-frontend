0016b0d0d885d6da28d638f310c6d154d3deb645
albertsube <albert.sune.belmonte@gmail.com>
2023-12-29 14:48:13 +0100

    Recipe page styles

--- src/pages/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe.jsx
@@ -1,7 +1,6 @@
 import useRecipe from '../../hooks/useRecipe'
 import styles from './Recipe.module.css'
 import { useParams } from 'react-router-dom'
-import { difficultyColor, preparationTimeColor, categoryColor } from '../../../public/colors/recipe.colors'
 import { FETCH_STATUS } from '../../constants/fetchStatus'
 
 export default function Recipe() {
@@ -19,38 +18,45 @@ export default function Recipe() {
   }
 
   return (
-    <div className={styles.test}>
+    <div>
+      <div className={styles.imageWrapper} style={{ backgroundImage: `url(${imageUrl})` }}>
+        {/* <img className={styles.image} src={imageUrl} alt="recipe-image" /> */}
+        <div className={styles.imageCover}>
+          <p>{difficulty}</p>
+          <p>{preparationTime}</p>
+        </div>
+
+        <div className={styles.categories}>
+          {categories.map((category) => (
+            <span key={category}>{category}</span>
+          ))}
+        </div>
+      </div>
+
       <h2 className={styles.title}>{title}</h2>
 
-      <div className={styles.imageWrapper}>
-        <img src={imageUrl} alt="recipe-image" />
-      </div>
-
-      <div className={styles.complexity}>
-        <span style={{ backgroundColor: difficultyColor[difficulty] }}>{difficulty}</span>
-        <span style={{ backgroundColor: preparationTimeColor[preparationTime] }}>{preparationTime}</span>
-      </div>
-
-      <div className={styles.categories}>
-        {categories.map((category) => (
-          <span key={category} className={styles.category} style={{ backgroundColor: categoryColor[category] }}>
-            {category}
-          </span>
-        ))}
-      </div>
-
-      <ul>
-        {ingredients.map((ingredient) => (
-          <li key={ingredient}>{ingredient}</li>
-        ))}
-      </ul>
-
-      {preparation.map(({ title, description }, i) => (
-        <div key={i}>
-          <h3>{`Step ${i}: ${title}`}</h3>
-          <p>{description}</p>
+      <div className={styles.contentWrapper}>
+        <div>
+          <h3 className={styles.sectionTitle}>Ingredients</h3>
+          <ul className={styles.ingredients}>
+            {ingredients.map((ingredient) => (
+              <li key={ingredient}>{ingredient}</li>
+            ))}
+          </ul>
         </div>
-      ))}
+
+        <div className={styles.steps}>
+          {preparation.map(({ title, description }, i) => (
+            <div key={i}>
+              <h3 className={styles.sectionTitle}>{i + 1}</h3>
+              <div className={styles.stepContent}>
+                <h4>{title}</h4>
+                <p>{description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
