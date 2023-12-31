a0907992686c052143e560f890c90fcbfd678550
albertsube <albert.sune.belmonte@gmail.com>
2023-12-31 13:31:36 +0100

    Basic profile styles

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,13 +1,12 @@
 import styles from './Profile.module.css'
 import { useParams } from 'react-router-dom'
-import { useState, useEffect } from 'react'
 import { FETCH_STATE } from '../../constants/fetchState'
-import { getUserById } from '../../api/user'
 import useUser from '../../hooks/useUser'
 
 export default function Profile() {
   const { id } = useParams()
   const { user, state, error } = useUser(id)
+  const { name, lastName, userName, imageUrl, email, country, description, recipes } = user
 
   if (state === FETCH_STATE.LOADING) {
     return <div>Loading...</div>
@@ -18,14 +17,29 @@ export default function Profile() {
   }
 
   return (
-    <div>
-      <h2>
-        {user.name} {user.lastName}
-      </h2>
-      <img src={user.imageUrl} alt="profile-image" />
-      <p>{user.email}</p>
-      <p>{user.userName}</p>
-      <p>{user.country}</p>
+    <div className={styles.profile}>
+      <aside className={styles.sideBar}>
+        <div className={styles.imageWrapper} style={{ backgroundImage: `url(${imageUrl})` }} />
+        <h2 className={styles.name}>
+          {name} {lastName}
+        </h2>
+        <p className={styles.userName}>@{userName}</p>
+        <p className={styles.email}>{email}</p>
+        <p className={styles.country}>{country}</p>
+      </aside>
+
+      <main className={styles.mainContent}>
+        <div className={styles.descriptionWrapper}>
+          <h3 className={styles.sectionTitle}>About Me</h3>
+          <p>{description}</p>
+        </div>
+        <div className={styles.recipesWrapper}>
+          <h3 className={styles.sectionTitle}>Recipes</h3>
+          {recipes.map((recipe) => (
+            <p>{recipe.title}</p>
+          ))}
+        </div>
+      </main>
     </div>
   )
 }
