328c5fe3da5d019f88dd135e47ee314ca3f8b320
albertsube <albert.sune.belmonte@gmail.com>
2024-02-12 22:07:45 +0100

    Styling

--- src/components/FormField/FormField.jsx
+++ src/components/FormField/FormField.jsx
@@ -1,15 +1,16 @@
 import styles from './FormField.module.css'
 
 export default function FormField({ label, value, setValue, type = 'text', isEdit = true }) {
-  // console.log(value)
   return (
     <div className={styles.field}>
       <p className={styles.fieldTitle}>{label}</p>
-      {isEdit ? (
-        <input className={styles.fieldInput} type={type} value={value} onChange={(e) => setValue(e.target.value)} />
-      ) : (
-        <p className={styles.fieldData}>{value}</p>
-      )}
+      <input
+        className={styles.fieldInput}
+        type={type}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        disabled={!isEdit}
+      />
     </div>
   )
 }
