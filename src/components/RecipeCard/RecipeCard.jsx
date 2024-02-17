d6b723b20d90547cac219e628f0364a75a1c3483
albertsube <albert.sune.belmonte@gmail.com>
2024-02-17 09:39:10 +0100

    Correct favorite recipe card

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,12 +1,35 @@
+import { useState } from 'react'
+import { useProfile } from '../../context/ProfileContext'
 import styles from './RecipeCard.module.css'
 import { Link } from 'react-router-dom'
+import Heart from '../Heart'
 
 const RecipeCard = ({ recipe }) => {
   const formattedIngredients = recipe.ingredients.slice(0, -1).join(', ') + ', ' + recipe.ingredients.slice(-1)[0] + '.'
 
+  function Favorite() {
+    const { profile, toggleFavorite } = useProfile()
+
+    if (!profile) return
+
+    const [isFavorite, setIsFavorite] = useState(profile.favRecipes.includes(recipe._id))
+
+    const handleClick = async (id) => {
+      await toggleFavorite(id)
+      setIsFavorite((f) => !f)
+    }
+
+    return (
+      <button onClick={() => handleClick(recipe._id)} className={styles.favoriteButton}>
+        <Heart favorite={isFavorite} />
+      </button>
+    )
+  }
+
   return (
     <div className={styles.recipeWrapper}>
       <div className={styles.recipeTop}>
+        <Favorite />
         <img className={styles.image} src={recipe.imageUrl} alt="recipe-image" />
       </div>
       <div className={styles.recipeContent}>
