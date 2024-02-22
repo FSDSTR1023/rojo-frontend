ab9c98ddcf16b3af89c9554a090572efbd2d6a83
albertsube <albert.sune.belmonte@gmail.com>
2024-02-22 20:17:10 +0100

    Loading error fixed

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -4,12 +4,16 @@ import ProfileCard from '../../components/ProfileCard'
 import { useParams } from 'react-router-dom'
 import useUser from '../../hooks/useUser'
 import { useState } from 'react'
+import { FETCH_STATE } from '../../constants/fetchState'
 
 export default function Profile() {
   const { id } = useParams()
-  const { user, setValue, getUser } = useUser(id)
+  const { user, setValue, getUser, state } = useUser(id)
   const [editing, setEditing] = useState(false)
   const [newProfileImage, setNewProfileImage] = useState(null)
+
+  if (state === FETCH_STATE.LOADING) return <p>Loading...</p>
+  if (state === FETCH_STATE.ERROR) return <p>Error!</p>
 
   return (
     <div className={styles.profile}>
