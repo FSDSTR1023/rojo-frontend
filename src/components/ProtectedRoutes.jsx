7ca42e0a703e9990a29d689a65e6f28bcc0d21ed
albertsube <albert.sune.belmonte@gmail.com>
2024-02-24 12:51:51 +0100

    Hot fix: glinch on protected routes

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -6,15 +6,11 @@ import { FETCH_STATE } from '../constants/fetchState'
 export default function ProtectedRoutes() {
   const { isAuthenticated, state } = useProfile()
 
-  if (state === FETCH_STATE.LOADING) {
-    return <div>Loading...</div>
-  }
-
   if (state === FETCH_STATE.ERROR) {
     return <Navigate to="/login" replace />
   }
 
-  if (!isAuthenticated) return <Navigate to="/login" replace />
+  if (!isAuthenticated && state !== FETCH_STATE.LOADING) return <Navigate to="/login" replace />
 
   return <Outlet />
 }
